3c50d97435f396a457af5f26bcf68e06cacb6792
Francesco Giordano <francescogior@users.noreply.github.com>
2020-11-02 13:07:39 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,253 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { Global as GlobalStyle, css } from "@emotion/core";
+
+const GLOBAL_STYLE = css`
+  body {
+    margin: 0;
+    padding: 0;
+  }
+`;
+const DICE_SIZE = 50;
+const DICE_MARGIN = 10;
+
+type DiceValue = 1 | 2 | 3 | 4 | 5 | 6;
+
+const rollDice = (): DiceValue =>
+  (Math.floor(Math.random() * 6) + 1) as DiceValue;
+
+type DiceProps = {
+  color: string;
+  isDisabled: boolean;
+  isTappable: boolean;
+  isBordered: boolean;
+};
+
+const DiceShape = styled.div(
+  ({ color, isDisabled, isTappable, isBordered }: DiceProps) => ({
+    width: DICE_SIZE,
+    height: DICE_SIZE,
+    background: color,
+    borderRadius: 4,
+    display: "flex",
+    alignItems: "center",
+    color: "white",
+    margin: DICE_MARGIN,
+    outline: "none",
+    cursor: isTappable ? "pointer" : "default",
+    opacity: isDisabled ? 0.5 : 1,
+    pointerEvents: isTappable ? undefined : "none",
+    userSelect: "none",
+    transition: "transform .3s",
+    "@media(hover: hover)": {
+      ":hover": { opacity: isTappable ? 0.6 : 0.9 }
+    },
+    ":active": {
+      opacity: isTappable ? 0.7 : 0.8,
+      transform: "scale(.98)"
+    },
+    boxSizing: "border-box",
+    border: isBordered ? "6px solid black" : "unset"
+  })
+);
+
+const DiceValueDisplay = styled.div({
+  width: "100%",
+  textAlign: "center",
+  color: "white",
+  fontWeight: 900,
+  fontSize: DICE_SIZE * 0.7,
+  fontFamily: "sans-serif"
+});
+
+const Board = styled.div({
+  background: "#dee8d5",
+  height: "100vh",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center"
+});
+
+const Side = styled.div({
+  display: "flex"
+});
+
+const RollButton = styled.div({
+  width: 3 * DICE_SIZE + 4 * DICE_MARGIN,
+  height: DICE_SIZE,
+  lineHeight: `${DICE_SIZE}px`,
+  textAlign: "center",
+  margin: DICE_MARGIN,
+  cursor: "pointer",
+  fontFamily: "sans-serif",
+  color: "#32281f",
+  background: "#d5c7bc",
+  textTransform: "uppercase",
+  fontWeight: 900,
+  borderRadius: 4,
+  userSelect: "none",
+  fontSize: 36,
+  transition: "transform .3s",
+  "@media(hover: hover)": {
+    ":hover": { opacity: 0.9 }
+  },
+  ":active": {
+    opacity: 0.8,
+    transform: "scale(.98)"
+  }
+});
+
+type RisikoDiceType = "defense" | "attack";
+type RisikoDiceProps = {
+  type: RisikoDiceType;
+  value: DiceValue;
+  onClick: () => void;
+  isDisabled: boolean;
+  isToggable: boolean;
+  isWinning: boolean;
+};
+
+const risikoDiceTypeToColor: Record<RisikoDiceType, string> = {
+  defense: "#2f49b1",
+  attack: "#ff3f00"
+};
+
+function RisikoDice({
+  type,
+  value,
+  onClick,
+  isDisabled,
+  isToggable,
+  isWinning
+}: RisikoDiceProps) {
+  return (
+    <DiceShape
+      onClick={onClick}
+      color={risikoDiceTypeToColor[type]}
+      isDisabled={isDisabled}
+      isTappable={isToggable}
+      isBordered={isWinning}
+    >
+      <DiceValueDisplay>{value || "-"}</DiceValueDisplay>
+    </DiceShape>
+  );
+}
+
+type NumberOfDices = 1 | 2 | 3;
+
+function rollRisikoDices(numberOfDices: NumberOfDices) {
+  return [rollDice(), rollDice(), rollDice()]
+    .slice(0, numberOfDices)
+    .sort((a, b) => b - a);
+}
+
+export default function App() {
+  const [numberOfAttackers, setNumberOfAttackers] = React.useState<
+    NumberOfDices
+  >(3);
+  const [numberOfDefenders, setNumberOfDefenders] = React.useState<
+    NumberOfDices
+  >(3);
+  const [rolls, rollTheDices] = React.useState(0);
+  const [a1, a2, a3] = React.useMemo(
+    () => rollRisikoDices(numberOfAttackers),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [rolls]
+  );
+
+  const [d1, d2, d3] = React.useMemo(
+    () => rollRisikoDices(numberOfDefenders),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [rolls]
+  );
+
+  const toggleActive = React.useCallback(
+    (sideType: RisikoDiceType, diceIndex: number) => () => {
+      switch (sideType) {
+        case "attack":
+          switch (numberOfAttackers) {
+            case 1:
+              if (diceIndex === 1) setNumberOfAttackers(2);
+              break;
+            case 2:
+              if (diceIndex === 1) setNumberOfAttackers(1);
+              if (diceIndex === 2) setNumberOfAttackers(3);
+              break;
+            case 3: {
+              if (diceIndex === 2) setNumberOfAttackers(2);
+            }
+          }
+          break;
+        case "defense": {
+          switch (numberOfDefenders) {
+            case 1:
+              if (diceIndex === 1) setNumberOfDefenders(2);
+              break;
+            case 2:
+              if (diceIndex === 1) setNumberOfDefenders(1);
+              if (diceIndex === 2) setNumberOfDefenders(3);
+              break;
+            case 3: {
+              if (diceIndex === 2) setNumberOfDefenders(2);
+            }
+          }
+        }
+      }
+    },
+    [numberOfAttackers, numberOfDefenders]
+  );
+
+  return (
+    <Board>
+      <GlobalStyle styles={GLOBAL_STYLE} />
+      <Side>
+        {[a1, a2, a3].map((value, diceIndex) => (
+          <RisikoDice
+            type="attack"
+            isDisabled={diceIndex + 1 > numberOfAttackers}
+            value={value}
+            key={diceIndex}
+            isWinning={value > [d1, d2, d3][diceIndex]}
+            onClick={toggleActive("attack", diceIndex)}
+            isToggable={
+              (numberOfAttackers === 1 && diceIndex === 1) ||
+              (numberOfAttackers === 2 &&
+                (diceIndex === 1 || diceIndex === 2)) ||
+              (numberOfAttackers === 3 && diceIndex === 2)
+            }
+          />
+        ))}
+      </Side>
+
+      <Side>
+        {[d1, d2, d3].map((value, diceIndex) => (
+          <RisikoDice
+            type="defense"
+            value={value}
+            isWinning={value >= [a1, a2, a3][diceIndex]}
+            isDisabled={diceIndex + 1 > numberOfDefenders}
+            key={diceIndex}
+            onClick={toggleActive("defense", diceIndex)}
+            isToggable={
+              (numberOfDefenders === 1 && diceIndex === 1) ||
+              (numberOfDefenders === 2 &&
+                (diceIndex === 1 || diceIndex === 2)) ||
+              (numberOfDefenders === 3 && diceIndex === 2)
+            }
+          />
+        ))}
+      </Side>
+
+      <RollButton
+        onClick={() => {
+          rollTheDices(rolls + 1);
+        }}
+      >
+        <span role="img" aria-label="dice">
+          🎲
+        </span>
+      </RollButton>
+    </Board>
+  );
+}
